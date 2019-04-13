e2e0ac59d3647cfda915cd2d628dc98aa797269a
Aero558 <49406547+Aero558@users.noreply.github.com>
2019-04-14 03:57:21 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 随机出题_四则运算_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        proplem p = new proplem();
+        int result;
+        string str = "";
+        int n;
+        int a = 0, b = 0, c = 0;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = Convert.ToString(0);
+            double s;
+            textBox1.Clear();
+            s = 100 * b / a;
+            listBox1.Items.Add("题目数：" + a + ",正确题目数：" + b + ",错误题目数：" + c);
+            listBox1.Items.Add("你的正确率：" + Math.Round(s, 0) +"%");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form1_Load(sender, e);
+            listBox1.Items.Clear();
+            a = 0;
+            b = 0;
+            c = 0;
+        }
+
+        private void textBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox2.Clear();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            str = p.Proplem(str, out result);
+            textBox1.Text = str;
+            textBox2.Text =Convert.ToString(0);
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            n = Convert.ToInt32(textBox2.Text);
+            if (result == n)
+             { 
+                listBox1.Items.Add(str + textBox2.Text +" "+ "正确");
+                a++;
+                b++;
+            }
+            else
+            {
+                listBox1.Items.Add(str + textBox2.Text +" "+ "错误");
+                a++;
+                c++;
+            }
+            Form1_Load(sender, e);
+            textBox2.Text = Convert.ToString(0);
+        }
+
+    }
+}
---
+++ proplem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 随机出题_四则运算_
+{
+    class proplem
+    {
+        Random rnd = new Random();
+        public int a;
+        public int b;
+        public int c;
+        public int d;
+        public string Proplem(string str,out int result)
+        {
+            a = rnd.Next(1, 5);
+            b = rnd.Next(20);
+            c = rnd.Next(20);
+            d = rnd.Next(20);
+            if (a == 1)
+            {
+                str = str = Convert.ToString(b +"+"+ c+"-"+d+"="); ;
+                result = b + c-d;
+            }
+            else if (a == 2 && b > c)
+            {
+                str = Convert.ToString(b + "-" + c + "+"+d+"=");
+                result = b - c+d;
+            }
+            else if (a == 4 && c != 0 && b % c == 0)
+            {
+                str =Convert.ToString(b + "/" + c +"+"+d+ "=");
+                result = b / c+d;
+            }
+            else
+            {
+                str = Convert.ToString(b + "*" + c + "-"+d+"=");
+                result = b * c-d;
+            }
+            return str;
+        }
+       
+    }
+}
